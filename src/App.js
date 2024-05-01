551085efac06842db4b9e5303039c380963938e6
Tushank01 <tushankqwe@gmail.com>
2024-05-01 23:26:17 +0530

    Filters added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 import Card from './Card.js'
 import { useState } from 'react';
 import { useEffect } from 'react';
+// import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 
 function App() {
+  const [Role, setRole]= useState("");
+  const [NumberofEmployee, setNumberofEmployee]= useState("");
+  const [Experience, setExperience]= useState("");
+  const [Location, setLocation]= useState("");
+  const [MinBasePay, setMinBasePay]= useState("");
+  const [Search, setSearch]= useState("");
+
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
@@ -13,7 +21,7 @@ function App() {
       myHeaders.append("Content-Type", "application/json");
 
       const raw = JSON.stringify({
-        "limit": 10,
+        "limit": 100,
         "offset": 0
       });
 
@@ -40,98 +48,50 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <select className="dropdown  role">
-          <option value="">Role</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          {/* Add more options here */}
+        <select onChange={(e)=>setRole(e.target.value)} className="dropdown  role">
+          <option value="" disabled hidden selected >Role</option>
+          <option value="">All</option>
+          <option value="android">android</option>
+          <option value="frontend" >frontend</option>
+          <option value="ios"  >ios</option>
+          <option value="backend"  >backend</option>
+          <option value="tech lead"  >tech lead</option>
+
         </select>
         <select className="dropdown empl_num ">
-          <option value="">Number of Employees</option>
+          <option value="" disabled hidden selected >Number of Employees</option>
           <option value="">Dropdown 1</option>
           <option value="">Dropdown 1</option>
           <option value="">Dropdown 1</option>
           <option value="">Dropdown 1</option>
           <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
+
           {/* Add more options here */}
         </select>
         <select className="dropdown">
-          <option value="">Experince</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
+          <option value="" disabled hidden selected >Experince</option>
           <option value="">Dropdown 1</option>
           <option value="">Dropdown 1</option>
           {/* Add more options here */}
         </select>
-        <select className="dropdown">
-          <option value="">Remote</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          {/* Add more options here */}
+        <select onChange={(e)=>setLocation(e.target.value)} className="dropdown">
+          <option value="" disabled hidden selected >Location</option>
+          <option value="">All</option>
+          <option value="mumbai"  >mumbai</option>
+          <option value="bangalore" >bangalore</option>
+          <option value="delhi ncr"   >delhi ncr</option>
+          <option value="chennai"   >chennai</option>
+          <option value="remote"  >remote</option>
         </select>
+
         <select className="dropdown empl_num ">
-          <option value="">min base pay salary</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
-          <option value="">Dropdown 1</option>
+          <option value="" disabled hidden selected >min base pay salary</option>
           <option value="">Dropdown 1</option>
           <option value="">Dropdown 1</option>
           {/* Add more options here */}
         </select>
         <div className="search-container">
-          <input type="text" className="search" placeholder="Search..." />
-          <button type="submit" className="search-button">
-            <i className="fa fa-search"></i>
-          </button>
+          <input value={Search} onChange={(e)=>setSearch(e.target.value)}  type="text" className="search" placeholder="Search..." />
         </div>
 
       </header>
@@ -139,7 +99,12 @@ function App() {
       </br>
       <div className="cards-container">
       <div className="card-row">
-      {jobs.map(job => (
+      {jobs
+      .filter(job=>  ( Role==="" || job.jobRole===Role))
+      .filter(job=> (Location==="" || job.location===Location))
+      .filter(job=> (job.jdUid.includes(Search)))
+      
+      .map(job => (
             <Card
               key={job.jdUid}
               Name={job.jdUid}
@@ -147,6 +112,7 @@ function App() {
               content={`Job Description: ${job.jobDetailsFromCompany}`}
               location={job.location}
               buttonText="Apply"
+              buttonText2="Referral"
             />
           ))}
            </div>
--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,23 @@
 import React from 'react';
+// import { IconButton } from '@mui/material';
+// import {ElectricBoltIcon} from '@mui/icons-material/ElectricBolt'
 
 
-function Card({ Name, title, content, location, buttonText }) {
+function Card({ Name, title, content, location, buttonText,buttonText2 }) {
   return (
     <div className="card">
-      {/* <h3>{title}</h3> */}
-      <h3>{Name}</h3>
+      
+      <h3 className="Job-id" >{Name}</h3>
+      <h3 className="card-jobrole" >{title}</h3>
+      
       <div className="card-content">
-        <p><strong>Location:</strong> {location}</p>
-        <p>{content}</p>
+        <h3 className="location" ><strong>Location:</strong> {location}</h3>
+        <p className='Job-content' >{content}</p>
         <div className="card-gradient"></div> {/* Add gradient overlay */}
       </div>
-      <button className="custom-button green-button">{buttonText}</button>
+      <button className="custom-button green-button" >{buttonText}</button>
+      <button className="custom-button blue-button">{buttonText2}</button>
+
     </div>
   );
 }
