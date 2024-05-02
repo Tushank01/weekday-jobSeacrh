c22a48559711eca53d48b000ca87c8d11374f5a5
Tushank01 <tushankqwe@gmail.com>
2024-05-02 22:45:59 +0530

    components indroduction

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchJobDetails } from './store/detailsReducer';
 import jobImage from './1630638631345.jpeg'; // Import the image here
+import Select from 'react-select';
+import BoltIcon from '@mui/icons-material/Bolt';
 
 function Card({ Name, title, content, location, experience, minPay, maxPay,buttonText, buttonText2 }) {
   const dispatch = useDispatch();
@@ -25,40 +27,41 @@ function Card({ Name, title, content, location, experience, minPay, maxPay,butto
 
   const displaySalary = minPay !== null ? `Estimated Salary: ₹${minPay} - ₹${maxPay} LPA ✅` : `Estimated Salary: Up to ₹${maxPay} LPA ✅`;
 
+  
 
   return (
     <div className="card">
       <div className="grid-container">
         <div className="grid-item">
           <img src={jobImage} alt="Job Image" className="job-image" />
-          
         </div>
         
         <div className="grid-item">
           <h3 className="Job-id">{Name}</h3>
           <h3 className="card-jobrole">{title}</h3>
-          <h3 className="location"><strong>Location:</strong> {location}</h3>
+          <h3 className="location">{location}</h3>
         </div>
-        <p className='estimate-salary' >{displaySalary}</p>
+        <p className='estimate-salary'>{displaySalary}</p>
       </div>
-      <h3 className="about-company" >About Company: </h3>
+      <h3 className="about-company">About Company: </h3>
       <h5 className="about-us"> About us</h5>
       <div className="card-content">
         
         
         <p className='Job-content'>{content}</p>
-        <p className='view-jd' >View job</p>
+        <p className='view-jd'   >View job</p>
         <div className="card-gradient"></div> {/* Add gradient overlay */}
       </div>
       <p className='minpay'>{minPay}</p>
       {experience !== null && (
         <div>
-          {/* <p className='view-jd' >view</p> */}
+          
           <h3 className='minimum-exp'>Minimum Experience</h3>
           <p className="experience">{experience} years</p>
         </div>
       )}
-      <button className="custom-button green-button" onClick={handleClick}>{buttonText}</button>
+      <button className="custom-button green-button" onClick={handleClick}>{buttonText} </button>
+      
       <button className="custom-button blue-button">{buttonText2}</button>
     </div>
   );
--- src/ss.js
+++ src/ss.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card from './Card.js';
 import Select from 'react-select';
-import Dropdown from "./Dropdowns.js";
+
+
 
 function App() {
   const [Role, setRole] = useState("");
@@ -44,54 +45,6 @@ function App() {
   }, []);
 
 
-  const roleOptions = [
-    { value: 'android', label: 'android' },
-    { value: 'ios', label: 'ios' },
-    { value: 'tech lead', label: 'tech lead' },
-    { value: 'backend', label: 'backend' },
-    { value: 'frontend', label: 'frontend' }
-  ]
-
-   const expOptions = [
-    { value: '1', label: '1 year' },
-    { value: '2', label: '2 years' },
-    { value: '3', label: '3 years' },
-    { value: '4', label: '4 years' },
-    { value: '5', label: '5 years' },
-    { value: '6', label: '6 years' },
-    { value: '7', label: '7 years' },
-    { value: '8', label: '8 years' },
-    { value: '9', label: '9 years' },
-    { value: '10', label: '10 years' }
-  ]
-
-    const locationOptions = [
-      { value: 'bangalore', label: 'Bangalore' },
-      { value: 'mumbai', label: 'Mumbai' },
-      { value: 'delhi ncr', label: 'Delhi NCR' },
-      { value: 'chennai', label: 'chennai' },
-      { value: 'remote', label: 'remote' }
-    ]
-
-    const minbaseOptions = [
-      { value: '0', label: '0 LPA' },
-      { value: '10', label: '10 LPA' },
-      { value: '20', label: '20 LPA' },
-      { value: '30', label: '30 LPA' },
-      { value: '40', label: '40 LPA' },
-      { value: '50', label: '50 LPA' },
-      { value: '60', label: '60 LPA' },
-      { value: '70', label: '70 LPA' },
-      { value: '80', label: '80 LPA' },
-      { value: '90', label: '90 LPA' },
-      { value: '100', label: '100 LPA' },
-      { value: '10', label: '100 LPA' },
-  
-  
-      // Add more options as needed
-    ]
-
-
   const handleRoleChange = (selectedOptions) => {
     setRole(selectedOptions.map(option => option.value));
   };
@@ -108,6 +61,7 @@ function App() {
   const handleMinBasePayChange = (selectedOptions) => {
     setSelectedMinBasePay(selectedOptions.map(option => option.value));
   };
+  
   
   
   // Custom styles for react-select
@@ -127,39 +81,110 @@ const customStyles = {
 };
 
 
+
   return (
     <div className="App">
       <header className="App-header">
-      <Dropdown value={Role} placeholder={"Role"} Options={roleOptions} SetValue={ (v)=> setRole(v) } /> 
-        
+        <Select
+          isMulti
+          options={[
+            { value: 'android', label: 'android' },
+            { value: 'ios', label: 'ios' },
+            { value: 'tech lead', label: 'tech lead' },
+            { value: 'backend', label: 'backend' },
+            { value: 'frontend', label: 'frontend' }
+          ]}
+          placeholder="Role"
+          onChange={handleRoleChange}
+          styles={customStyles} 
+        />
+        {/* Other dropdowns */}
         <select className="dropdown empl_num ">
           <option value="" disabled hidden selected >Number of Employees</option>
+          <option value="">Dropdown 1</option>
+          <option value="">Dropdown 1</option>
+          <option value="">Dropdown 1</option>
+          <option value="">Dropdown 1</option>
+          <option value="">Dropdown 1</option>
 
-
-          
+          {/* Add more options here */}
         </select>
-        <Dropdown value={Experience} placeholder={"Experience"} Options={expOptions} SetValue={ (v)=> setExperience(v) } />
-        <Dropdown value={Location} placeholder={"Location"} Options={locationOptions} SetValue={ (v)=> setLocation(v) } />
+        <Select
+  isMulti
+  options={[
+    { value: '1', label: '1 year' },
+    { value: '2', label: '2 years' },
+    { value: '3', label: '3 years' },
+    { value: '4', label: '4 years' },
+    { value: '5', label: '5 years' },
+    { value: '6', label: '6 years' },
+    { value: '7', label: '7 years' },
+    { value: '8', label: '8 years' },
+    { value: '9', label: '9 years' },
+    { value: '10', label: '10 years' }
+  ]}
+  placeholder="Experience"
+  onChange={handleExpChange}
+  styles={customStyles} 
+/>
 
-        <Dropdown value={MinBasePay} placeholder={"Min Base Pay"} Options={minbaseOptions} SetValue={ (v)=> setMinBasePay(v) } />
+        <Select
+          isMulti
+          options={[
+            { value: 'bangalore', label: 'Bangalore' },
+            { value: 'mumbai', label: 'Mumbai' },
+            { value: 'delhi ncr', label: 'Delhi NCR' },
+            { value: 'chennai', label: 'chennai' },
+            { value: 'remote', label: 'remote' }
+          ]}
+          placeholder="Location"
+          onChange={handleLocationChange}
+          styles={customStyles} 
+        />
 
+<Select
+  isMulti
+  options={[
+    { value: '0', label: '0 LPA' },
+    { value: '10', label: '10 LPA' },
+    { value: '20', label: '20 LPA' },
+    { value: '30', label: '30 LPA' },
+    { value: '40', label: '40 LPA' },
+    { value: '50', label: '50 LPA' },
+    { value: '60', label: '60 LPA' },
+    { value: '70', label: '70 LPA' },
+    { value: '80', label: '80 LPA' },
+    { value: '90', label: '90 LPA' },
+    { value: '100', label: '100 LPA' },
+    { value: '10', label: '100 LPA' },
+
+
+    // Add more options as needed
+  ]}
+  placeholder="Minimum Base Pay"
+  onChange={handleMinBasePayChange}
+  styles={customStyles}
+/>
         <div className="search-container">
           <input value={Search} onChange={(e) => setSearch(e.target.value)} type="text" className="search" placeholder="Search..." />
         </div>
       </header>
-      <br></br>``
+      <br></br>
       <div className="cards-container">
         <div className="card-row">
           {jobs
-            .filter(job => (Role.length === 0 || Role.some( role=>role.value ===  job.jobRole)))
-            .filter(job => (Experience.length === 0 || (job.minExp !== null && Experience.some( experience=> parseInt(experience.value) <= parseInt(job.minExp.toString())))))
-            .filter(job => (Location.length === 0 || Location.some( location=>location.value===   job.location)))
+            .filter(job => (Role.length === 0 || Role.includes(job.jobRole)))
+            .filter(job => (
+              (Experience.length === 0) ||
+              (job.minExp !== null && Experience.some(exp => parseInt(exp) >= parseInt(job.minExp)))
+            ))
+            
+            .filter(job => (Location.length === 0 || Location.includes(job.location)))
             .filter(job => (job.jdUid.includes(Search)))
             .filter(job => (
               (selectedMinBasePay.length === 0) ||
-              (job.minJdSalary && selectedMinBasePay.some( minpays=>minpays.value >= ( min => parseInt(min) <= parseInt(job.minJdSalary ))))
+              (job.minJdSalary && selectedMinBasePay.some(min => parseInt(min) <= parseInt(job.minJdSalary)))
             ))
-
             .map(job => (
               <Card
                 key={job.jdUid}
